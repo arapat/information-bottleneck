3a3a58ee46e5198dfe97c5801ec6c84cf8ee0d9d
Julaiti Alafate <jalafate@gmail.com>
2015-06-30 00:45:58 -0700

    fixed all bugs so far

--- entropy.py
+++ entropy.py
@@ -4,16 +4,16 @@ import numpy as np
 from scipy.stats import entropy
 
 def compute_entropy(p):
-    p.sort()
-    total = len(p)
-    sep = [i for i in range(total-1) if p[i] != p[i+1]]
+    sp = sorted(p)
+    total = len(sp)
+    sep = [i for i in range(total-1) if sp[i] != sp[i+1]]
     if not sep:
         return 1.0
     stats = [(sep[0] + 1.0)] + [sep[k+1] - sep[k] for k in range(len(sep)-1)] \
             + [total - sep[-1] - 1]
     return entropy(np.array(stats) / total, base = 2.0)
 
-def hard_clustering(p_cn_trans, is_leaf):
+def hard_clustering(p_cn_trans):
     return np.argmax(p_cn_trans, axis=0)
 
 def result_entropy(p_n, p_vn, p_vn_co_occur, init_beta, K, trials = 10):
@@ -24,7 +24,7 @@ def result_entropy(p_n, p_vn, p_vn_co_occur, init_beta, K, trials = 10):
     while loop < trials:
         p_cn_trans, child, is_leaf, free_energy, split_point, beta = distributional_clustering(p_n, p_vn, p_vn_co_occur, K, \
                                         beta = init_beta, delta = 0.5, convergeDist = 0.001, splitDist = 0.05, alpha = 0.1)
-        for k, c in zip(range(len(nouns)), hard_clustering(p_cn_trans, is_leaf)):
+        for k, c in zip(range(len(nouns)), hard_clustering(p_cn_trans)):
             result[k] = result[k] + '(' + str(c) + ')'
         p_entropy.append(compute_entropy(result))
         min_beta = min(min_beta, beta)
