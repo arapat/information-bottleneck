4de80c565df8ebb868ccff1ed1daf7b009f2e996
Julaiti Alafate <jalafate@gmail.com>
2015-07-31 14:32:30 -0700

    small fixes

--- sparse/sib.py
+++ sparse/sib.py
@@ -2,21 +2,20 @@ import numpy as np
 
 def hartigan_centroids(p_yx, assignments):
     centroids = p_yx.map(lambda (x, v): (assignments[x], (p_x[x], p_x[x] * v))) \
-                    .reduceByKey(lambda (a, b): (a[0] + b[0], a[1] + b[1])) \
+                    .reduceByKey(lambda (a, b): (a[0] + b[0], a[1] + b[1]))
     return map(lambda p: p[1], centroids)
 
 
 def hartigan_init(p_yx):
     def partition(v, p1, p2):
-        d1 = distance(v, p1)
-        d2 = distance(v, p2)
-        if d1 < d2:
+        d = distance(v.toarray(), np.array([p1, p2]))
+        if d[0] < d[1]:
             return 0
         return 1
 
-    p1 = p_yx.takeSample(False, 1)[1]
-    weights = p_yx.map(lambda (x, v): (x, distance(v, p1)) \
-                  .sortByKey().collect()
+    p1 = p_yx.takeSample(False, 1)[0][1]
+    weights = p_yx.map(lambda (x, v): (x, distance(v.toarray(), p1, True, True))) \
+                  .sortByKey().collect()[0]
     weights = np.array(weights)**2
     p2_idx = np.random.choice(weights.size, p = weights / weights.sum())
     p2 = p_yx.zipWithIndex() \
@@ -31,17 +30,21 @@ def hartigan_init(p_yx):
     return assignments, p_yt
 
 
-def hartigan_twoCentroids(p_x, p_yx):
+def hartigan_twoCentroids(p_x, p_yx, numOfX):
     assignments, p_yt = hartigan_init(p_yx)
     while True:
         new_assignments = p_yx.map(lambda (x, v): (x, adjust(v, p_yt, assignments[x]))) \
                               .sortByKey() \
                               .map(lambda p: p[1]) \
-                              .collect()
+                     )        .collect()
         new_assignments = np.array(new_assignments)
         if np.all(assignments == new_assignments):
             break
         assignments = new_assignments
         p_yt = hartigan_centroids(p_yx, assignments)
-    return p_yt
+
+    p_tx = np.zeros((numOfX, 2))
+    for k in range(numOfX):
+        p_tx[k][assignments[k]] = 1.0
+    return p_tx
 
